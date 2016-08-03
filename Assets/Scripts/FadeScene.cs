3c2a700d74608becd69db1d2d8fe6806613f2f88
GitHubAnother <1041820154@qq.com>
2016-08-03 18:17:09 +0800

    2

---
+++ Assets/Scripts/FadeScene.cs
@@ -0,0 +1,42 @@
+﻿/*
+作者名称:YHB
+
+脚本作用:实现淡入淡出的效果
+
+建立时间:2016.8.3.16.57
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class FadeScene : MonoBehaviour
+{
+    #region 字段
+    public Texture texture;
+    public float fadeSpeed = 0.5f;//淡入淡出的速度
+
+    private int fade = -1;//用来控制是淡入还是淡出的
+    private float alpha = 1;//透明度
+    #endregion
+
+    void OnGUI()
+    {
+        alpha += fade * fadeSpeed * Time.deltaTime;
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), texture);//绘制全图的图片
+    }
+
+    void OnLevelWasLoaded()//unity回调函数，场景被加载的时候会调用
+    {
+        FadeTime(-1);
+    }
+
+    //JK---返回淡入使用的时间
+    public float FadeTime(int fade)
+    {
+        this.fade = fade;
+        return 1 / fadeSpeed;
+    }
+
+}
---
+++ Assets/Scripts/SceneBtn.cs
@@ -0,0 +1,30 @@
+﻿/*
+作者名称:YHB
+
+脚本作用:OnGUI按钮切换场景
+
+建立时间:2016.8.3.16.53
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneBtn : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 100, 50), "切换场景"))
+        {
+            StartCoroutine(FadeScenes());
+        }
+    }
+
+    IEnumerator FadeScenes()
+    {
+        float timer = GameObject.Find("Fade").GetComponent<FadeScene>().FadeTime(1);
+        yield return new WaitForSeconds(timer);
+
+        SceneManager.LoadScene(1);
+    }
+}
